31563df3c9527934fdefb12fc4c0d032f1b000ca
Jay Dee <jay.developes@simplelogin.fr>
2021-04-02 23:39:21 +0200

    Add route constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const SETTINGS_ROUTE = '/settings';
+export const HOME_ROUTE = '/';
+export const PORTFOLIOS_ROUTE = '/portfolios';
+export const NEW_ROUTE = '/new';
+export const UPLOAD_ROUTE = '/upload';
+export const ABOUT_ROUTE = '/about';
