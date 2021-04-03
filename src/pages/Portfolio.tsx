e2f03ab0858aa785a6c12b902bd91d8cc9dcac4e
Jay Dee <jay.developes@simplelogin.fr>
2021-04-03 23:05:29 +0200

    Implement invalid portfolio error handling

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Layout } from '../components/Layout';
+import NotFound from './NotFound';
+import { getPortfolioByName } from '../services/portfolio';
 
 interface Props {
   match: any;
@@ -7,6 +9,12 @@ interface Props {
 
 export default function Portfolio({ match }: Props) {
   const { portfolioName } = match.params;
+  const portfolio =  getPortfolioByName(portfolioName);
+  console.log(portfolio)
+
+  if (!portfolio) {
+    return <NotFound/>;
+  }
 
   return (
     <Layout>
