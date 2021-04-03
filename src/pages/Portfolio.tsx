f48405139b75597ed1fbcc8ef59cfc3d7c431806
Jay Dee <jay.developes@simplelogin.fr>
2021-04-03 23:25:51 +0200

    Fetch portfolio balances

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout } from '../components/Layout';
 import NotFound from './NotFound';
 import { getPortfolioByName } from '../services/portfolio';
+import { getAccount } from '../api/defichain';
 
 interface Props {
   match: any;
 }
 
 export default function Portfolio({ match }: Props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [balance, setBalance] = useState<any[]>([]);
   const { portfolioName } = match.params;
-  const portfolio =  getPortfolioByName(portfolioName);
-  console.log(portfolio)
+  const portfolio: any = getPortfolioByName(portfolioName);
+
+  const loadData = async () => {
+    const responses = [];
+    for (let index in portfolio.addresses) {
+      responses.push(await getAccount(portfolio.addresses[index]));
+    }
+
+    setBalance(responses);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   if (!portfolio) {
     return <NotFound/>;
@@ -18,7 +34,9 @@ export default function Portfolio({ match }: Props) {
 
   return (
     <Layout>
-      Portfolio { portfolioName }
+      {
+        isLoading ? 'Loading...' : `Portfolio: ${portfolio.name}`
+      }
     </Layout>
   );
 }
